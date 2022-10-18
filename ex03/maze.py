dd6aca3d943d96acd2adbb67134f95499bb6916b
C0B21067 <c0b21067f1@edu.teu.ac.jp>
2022-10-18 16:55:25 +0900

    追加機能

--- ex03/maze.py
+++ ex03/maze.py
@@ -11,27 +11,47 @@ def key_up(event):
 
 def main_proc():
     global cx, cy
+    global mx, my
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key =="Right":
-        cx += 20
+        mx += 1
+    if maze_lst[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key =="Right":
+            mx -= 1
+    #追加機能：残像を残すボタン
+    if key == "z":
+        canv.create_image(cx, cy, image = tori, tag="tori")
+
     canv.coords("tori", cx, cy)
     root.after(100,main_proc)
     
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.title("迷えるこうかとん")
+    root.title("迷えるコウカトン")
 
     canv = tk.Canvas(root,width = 1500, height = 900, bg = "black")
     canv.pack()
 
-    tori = tk.PhotoImage(file = "fig/5.png")
-    cx, cy = 300, 400
+    maze_lst = mm.make_maze(15,9)
+    mm.show_maze(canv, maze_lst)
+
+    tori = tk.PhotoImage(file = "fig/9.png")
+    mx, my = 1, 1
+    cx, cy = mx*100+50, my*100+50
     canv.create_image(cx, cy, image = tori, tag="tori")
 
     key = ""
@@ -40,11 +60,4 @@ if __name__ == "__main__":
     root.bind("<KeyRelease>", key_up)
 
     main_proc()
-    
-    maze_lst = mm.make_maze(15,9)
-    mm.show_maze(canv, maze_lst)
-
-
-
-
     root.mainloop()
